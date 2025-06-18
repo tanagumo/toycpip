fce4e66313c3f695d3226a42e55df008683ecb16
tanagumo <nagumo5683@gmail.com>
2025-06-18 21:28:30 +0900

    feat: implement `TcpPacket::new`

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::net::Ipv4Addr;
 
 use thiserror::Error;
 
@@ -60,27 +61,51 @@ impl Flags {
     }
 
     fn is_fin_flag_on(&self) -> bool {
-        self.0 & 0x01 == 1
+        self.fin_flag() == 1
+    }
+
+    fn fin_flag(&self) -> u8 {
+        self.0 & 0x01
     }
 
     fn is_syn_flag_on(&self) -> bool {
-        (self.0 >> 1) & 0x01 == 1
+        self.syn_flag() == 1
+    }
+
+    fn syn_flag(&self) -> u8 {
+        (self.0 >> 1) & 0x01
     }
 
     fn is_rst_flag_on(&self) -> bool {
-        (self.0 >> 2) & 0x01 == 1
+        self.rst_flag() == 1
+    }
+
+    fn rst_flag(&self) -> u8 {
+        (self.0 >> 2) & 0x01
     }
 
     fn is_psh_flag_on(&self) -> bool {
-        (self.0 >> 3) & 0x01 == 1
+        self.psh_flag() == 1
+    }
+
+    fn psh_flag(&self) -> u8 {
+        (self.0 >> 3) & 0x01
     }
 
     fn is_ack_flag_on(&self) -> bool {
-        (self.0 >> 4) & 0x01 == 1
+        self.ack_flag() == 1
+    }
+
+    fn ack_flag(&self) -> u8 {
+        (self.0 >> 4) & 0x01
     }
 
     fn is_urg_flag_on(&self) -> bool {
-        (self.0 >> 5) & 0x01 == 1
+        self.urg_flag() == 1
+    }
+
+    fn urg_flag(&self) -> u8 {
+        (self.0 >> 5) & 0x01
     }
 }
 
@@ -95,7 +120,189 @@ pub(crate) struct TcpPacket {
     window_size: u16,
     checksum: u16,
     urgent_ptr: u16,
+    option: Vec<u8>,
     payload: Vec<u8>,
+}
+
+impl TcpPacket {
+    fn new(
+        src_ip: Ipv4Addr,
+        dst_ip: Ipv4Addr,
+        protocol: Protocol,
+        src_port: u16,
+        dst_port: u16,
+        sequence: u32,
+        ack_no: u32,
+        flags: Flags,
+        window_size: u16,
+        urgent_ptr: u16,
+        option: Vec<u8>,
+        payload: Vec<u8>,
+    ) -> Self {
+        let offset = Offset::new((option.len() as u8 + 20) / 4);
+        let checksum = Self::calc_checksum(
+            src_ip,
+            dst_ip,
+            protocol,
+            src_port,
+            dst_port,
+            sequence,
+            ack_no,
+            offset,
+            flags,
+            window_size,
+            urgent_ptr,
+            &option,
+            &payload,
+        );
+
+        Self {
+            src_port,
+            dst_port,
+            sequence,
+            ack_no,
+            offset,
+            flags,
+            window_size,
+            checksum,
+            urgent_ptr,
+            option,
+            payload,
+        }
+    }
+
+    pub(crate) fn calc_checksum(
+        src_ip: Ipv4Addr,
+        dst_ip: Ipv4Addr,
+        protocol: Protocol,
+        src_port: u16,
+        dst_port: u16,
+        sequence: u32,
+        ack_no: u32,
+        offset: Offset,
+        flags: Flags,
+        window_size: u16,
+        urgent_ptr: u16,
+        option: &[u8],
+        payload: &[u8],
+    ) -> u16 {
+        if option.len() % 4 != 0 {
+            panic!(
+                "the length of `option` must be multiple of 4, but got {}",
+                option.len()
+            );
+        }
+
+        let header_len = option.len() + 20;
+        if header_len > 60 {
+            panic!(
+                "TCP header length exceeds maximum (60 bytes), got {}",
+                header_len
+            );
+        }
+
+        if offset.as_u8() * 4 != (option.len() as u8 + 20) {
+            panic!(
+                "the expected offset value is {}, but got {}",
+                (option.len() + 20) / 4,
+                offset.as_u8()
+            );
+        }
+        let src_ip_array = src_ip.octets();
+        let dst_ip_array = dst_ip.octets();
+        let tcp_payload_len_array =
+            (offset.as_u8() as u16 * 4 + payload.len() as u16).to_be_bytes();
+
+        let src_port_array = src_port.to_be_bytes();
+        let dst_port_array = dst_port.to_be_bytes();
+        let sequence_array = sequence.to_be_bytes();
+        let ack_no_array = ack_no.to_be_bytes();
+        let byte_at_17: u8 = offset.as_u8() << 4;
+        let byte_at_18: u8 = (flags.urg_flag() << 5)
+            | (flags.ack_flag() << 4)
+            | (flags.psh_flag() << 3)
+            | (flags.rst_flag() << 2)
+            | (flags.syn_flag() << 1)
+            | flags.fin_flag();
+        let window_size_array = window_size.to_be_bytes();
+        let urgent_ptr_array = urgent_ptr.to_be_bytes();
+
+        let mut vec_to_calc_checksum =
+            Vec::with_capacity(offset.as_u8() as usize * 4 + payload.len());
+        vec_to_calc_checksum.extend([
+            // ip pseudo header
+            src_ip_array[0],
+            src_ip_array[1],
+            src_ip_array[2],
+            src_ip_array[3],
+            dst_ip_array[0],
+            dst_ip_array[1],
+            dst_ip_array[2],
+            dst_ip_array[3],
+            0,
+            protocol.into(),
+            tcp_payload_len_array[0],
+            tcp_payload_len_array[1],
+            // tpc_header
+            src_port_array[0],
+            src_port_array[1],
+            dst_port_array[0],
+            dst_port_array[1],
+            sequence_array[0],
+            sequence_array[1],
+            sequence_array[2],
+            sequence_array[3],
+            ack_no_array[0],
+            ack_no_array[1],
+            ack_no_array[2],
+            ack_no_array[3],
+            byte_at_17,
+            byte_at_18,
+            window_size_array[0],
+            window_size_array[1],
+            0,
+            0,
+            urgent_ptr_array[0],
+            urgent_ptr_array[1],
+        ]);
+        vec_to_calc_checksum.extend(option);
+        vec_to_calc_checksum.extend(payload);
+        utils::calculate_checksum(&vec_to_calc_checksum, None).unwrap()
+    }
+
+    pub(crate) fn src_port(&self) -> u16 {
+        self.src_port
+    }
+    pub(crate) fn dst_port(&self) -> u16 {
+        self.dst_port
+    }
+    pub(crate) fn sequence(&self) -> u32 {
+        self.sequence
+    }
+    pub(crate) fn ack_no(&self) -> u32 {
+        self.ack_no
+    }
+    pub(crate) fn offset(&self) -> Offset {
+        self.offset
+    }
+    pub(crate) fn flags(&self) -> Flags {
+        self.flags
+    }
+    pub(crate) fn window_size(&self) -> u16 {
+        self.window_size
+    }
+    pub(crate) fn checksum(&self) -> u16 {
+        self.checksum
+    }
+    pub(crate) fn urgent_ptr(&self) -> u16 {
+        self.urgent_ptr
+    }
+    pub(crate) fn option(&self) -> &[u8] {
+        &self.option
+    }
+    pub(crate) fn payload(&self) -> &[u8] {
+        &self.payload
+    }
 }
 
 impl TryFrom<&IpPacket> for TcpPacket {
@@ -195,6 +402,7 @@ impl TryFrom<&IpPacket> for TcpPacket {
             window_size: u16::from_be_bytes([ip_payload[14], ip_payload[15]]),
             checksum: actual_checksum,
             urgent_ptr: u16::from_be_bytes([ip_payload[18], ip_payload[19]]),
+            option: tcp_option.to_vec(),
             payload: tcp_payload.to_vec(),
         })
     }
