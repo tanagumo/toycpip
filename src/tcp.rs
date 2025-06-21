b2ad1196614b9e171a4af3fd7a8feba4a91ffa0f
tanagumo <nagumo5683@gmail.com>
2025-06-21 09:22:59 +0900

    refactor: restructure TcpPacket construction and add WithSrcIp wrapper

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,4 +1,6 @@
+use std::any::type_name;
 use std::borrow::Cow;
+use std::fmt::Display;
 use std::net::Ipv4Addr;
 
 use thiserror::Error;
@@ -109,6 +111,31 @@ impl Flags {
     }
 }
 
+impl Display for Flags {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut flags = vec![];
+        if self.is_fin_flag_on() {
+            flags.push("FIN");
+        }
+        if self.is_syn_flag_on() {
+            flags.push("SYN");
+        }
+        if self.is_rst_flag_on() {
+            flags.push("RST");
+        }
+        if self.is_psh_flag_on() {
+            flags.push("PSH");
+        }
+        if self.is_ack_flag_on() {
+            flags.push("ACK");
+        }
+        if self.is_urg_flag_on() {
+            flags.push("URG");
+        }
+        write!(f, "Flags({})", flags.join("+"))
+    }
+}
+
 #[derive(Debug)]
 pub(crate) struct TcpPacket {
     src_port: u16,
@@ -124,38 +151,30 @@ pub(crate) struct TcpPacket {
     payload: Vec<u8>,
 }
 
+impl Display for TcpPacket {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "TcpPacket(src_port={}, dst_port={}, sequence={}, ack_no={}, flags={})",
+            self.src_port, self.dst_port, self.sequence, self.ack_no, self.flags,
+        )
+    }
+}
+
 impl TcpPacket {
     fn new(
-        src_ip: Ipv4Addr,
-        dst_ip: Ipv4Addr,
-        protocol: Protocol,
         src_port: u16,
         dst_port: u16,
         sequence: u32,
         ack_no: u32,
+        offset: Offset,
         flags: Flags,
         window_size: u16,
+        checksum: u16,
         urgent_ptr: u16,
         option: Vec<u8>,
         payload: Vec<u8>,
     ) -> Self {
-        let offset = Offset::new((option.len() as u8 + 20) / 4);
-        let checksum = Self::calc_checksum(
-            src_ip,
-            dst_ip,
-            protocol,
-            src_port,
-            dst_port,
-            sequence,
-            ack_no,
-            offset,
-            flags,
-            window_size,
-            urgent_ptr,
-            &option,
-            &payload,
-        );
-
         Self {
             src_port,
             dst_port,
@@ -171,7 +190,7 @@ impl TcpPacket {
         }
     }
 
-    pub(crate) fn calc_checksum(
+    fn calc_checksum(
         src_ip: Ipv4Addr,
         dst_ip: Ipv4Addr,
         protocol: Protocol,
@@ -352,7 +371,41 @@ impl TcpPacket {
     }
 }
 
-impl TryFrom<&IpPacket> for TcpPacket {
+#[derive(Debug)]
+pub(crate) struct WithSrcIp<T> {
+    src_ip: Ipv4Addr,
+    value: T,
+}
+
+impl<T> Display for WithSrcIp<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let type_name = type_name::<T>().split("::").last().unwrap_or("Unknown");
+        write!(
+            f,
+            "WithSrcIp<{}>(src_ip={}, value={})",
+            type_name, self.src_ip, self.value
+        )
+    }
+}
+
+impl<T> WithSrcIp<T> {
+    pub(crate) fn src_ip(&self) -> Ipv4Addr {
+        self.src_ip
+    }
+
+    pub(crate) fn value(&self) -> &T {
+        &self.value
+    }
+
+    pub(crate) fn into_value(self) -> T {
+        self.value
+    }
+}
+
+impl TryFrom<&IpPacket> for WithSrcIp<TcpPacket> {
     type Error = TcpPacketError;
 
     fn try_from(ip_packet: &IpPacket) -> Result<Self, Self::Error> {
@@ -420,7 +473,31 @@ impl TryFrom<&IpPacket> for TcpPacket {
         vec_to_calc_checksum.extend(tcp_option);
         vec_to_calc_checksum.extend(tcp_payload);
 
-        let calculated_checksum = utils::calculate_checksum(&vec_to_calc_checksum, None).unwrap();
+        let src_port = u16::from_be_bytes([ip_payload[0], ip_payload[1]]);
+        let dst_port = u16::from_be_bytes([ip_payload[2], ip_payload[3]]);
+        let sequence =
+            u32::from_be_bytes([ip_payload[4], ip_payload[5], ip_payload[6], ip_payload[7]]);
+        let ack_no =
+            u32::from_be_bytes([ip_payload[8], ip_payload[9], ip_payload[10], ip_payload[11]]);
+        let flags = Flags::new(ip_payload[13] & 0x3f);
+        let window_size = u16::from_be_bytes([ip_payload[14], ip_payload[15]]);
+        let urgent_ptr = u16::from_be_bytes([ip_payload[18], ip_payload[19]]);
+
+        let calculated_checksum = TcpPacket::calc_checksum(
+            ip_packet.src_ip(),
+            ip_packet.dst_ip(),
+            ip_packet.protocol(),
+            src_port,
+            dst_port,
+            sequence,
+            ack_no,
+            offset,
+            flags,
+            window_size,
+            urgent_ptr,
+            tcp_option,
+            tcp_payload,
+        );
         let actual_checksum = u16::from_be_bytes([ip_payload[16], ip_payload[17]]);
         if actual_checksum != calculated_checksum {
             return Err(TcpPacketError::ChecksumMismatch(
@@ -430,27 +507,20 @@ impl TryFrom<&IpPacket> for TcpPacket {
         }
 
         Ok(Self {
-            src_port: u16::from_be_bytes([ip_payload[0], ip_payload[1]]),
-            dst_port: u16::from_be_bytes([ip_payload[2], ip_payload[3]]),
-            sequence: u32::from_be_bytes([
-                ip_payload[4],
-                ip_payload[5],
-                ip_payload[6],
-                ip_payload[7],
-            ]),
-            ack_no: u32::from_be_bytes([
-                ip_payload[8],
-                ip_payload[9],
-                ip_payload[10],
-                ip_payload[11],
-            ]),
-            offset: Offset::new(ip_payload[12] >> 4),
-            flags: Flags::new(ip_payload[13] & 0x3f),
-            window_size: u16::from_be_bytes([ip_payload[14], ip_payload[15]]),
-            checksum: actual_checksum,
-            urgent_ptr: u16::from_be_bytes([ip_payload[18], ip_payload[19]]),
-            option: tcp_option.to_vec(),
-            payload: tcp_payload.to_vec(),
+            src_ip: ip_packet.src_ip(),
+            value: TcpPacket::new(
+                src_port,
+                dst_port,
+                sequence,
+                ack_no,
+                offset,
+                flags,
+                window_size,
+                calculated_checksum,
+                urgent_ptr,
+                tcp_option.to_vec(),
+                tcp_payload.to_vec(),
+            ),
         })
     }
 }
